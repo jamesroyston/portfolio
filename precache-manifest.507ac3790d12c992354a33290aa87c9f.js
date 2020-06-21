75b9e8b40007c42a3839c5cadecb66c2eaaafb34
James Royston <jamesroyston03@gmail.com>
2020-06-20 20:18:23 -0500

    Updates

---
+++ precache-manifest.507ac3790d12c992354a33290aa87c9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d419a98164d0965ce4486a756201323",
+    "url": "./index.html"
+  },
+  {
+    "revision": "45b8f7705a5e1053f51b",
+    "url": "./static/css/main.4b9f590e.chunk.css"
+  },
+  {
+    "revision": "adc9d4cbbd039c81dba0",
+    "url": "./static/js/2.34366fe9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.34366fe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45b8f7705a5e1053f51b",
+    "url": "./static/js/main.25e7a382.chunk.js"
+  },
+  {
+    "revision": "adcc13a51a421fe21964",
+    "url": "./static/js/runtime-main.3cc98a72.js"
+  }
+]);
