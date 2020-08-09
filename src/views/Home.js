dd654942fbe3a5d4172605a226bcdf8718a9e41e
James Royston <jamesroyston03@gmail.com>
2020-08-09 15:18:54 -0500

    moved some stuff around

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import profile from './profile.jpg';
-import styled from 'styled-components';
+import React from "react";
+import profile from "./profile.jpg";
+import styled from "styled-components";
 
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  padding: 3rem 2rem;
   font-size: 18px;
 `;
 
 const Body = styled.div`
+  padding: 3rem 0;
   margin: 0 auto;
   width: 35rem;
 
   @media (max-width: 700px) {
-
-  width: 90%;
+    width: 90%;
   }
 `;
 
@@ -24,48 +23,65 @@ const Img = styled.img`
 `;
 
 export default function Home() {
-    return (
-        <Container>
-            <Body>
+  return (
+    <Container>
+      <Body>
+        <Img src={profile} alt="me" />
+        <h1>
+          I'm James Royston{" "}
+          <span role="img" aria-label="waving emoji">
+            👋
+          </span>
+        </h1>
 
-                <Img src={profile} alt="me"/>
-                <h1>I'm James Royston <span role="img" aria-label="waving emoji">👋</span></h1>
-                <p><small>Follow me: </small></p>
-                <small>
+        <p>
+          I'm a software engineer in the midwest, working on React.js
+          applications at{" "}
+          <span>
+            <a href="https://anautics.com/">Anautics</a>.{" "}
+          </span>
+          My favorite technologies right now are{" "}
+          <a href="https://reactjs.org/">React.js</a>,{" "}
+          <a href="https://styled-components.com/">styled components</a>, and{" "}
+          <a href="https://expressjs.com/">Express + Node.js</a>.
+        </p>
 
-                    <ul>
-                        <li>
-                            <a href="https://github.com/jamesroyston">Github</a>
-                        </li>
-                        <li>
-                            <a href="https://linkedin.com/in/jtroyston">LinkedIn</a>
-                        </li>
-                        <li>
-                            <a href="https://dev.to/jamesroyston">Dev.to</a>
-                        </li>
-                    </ul>
-                </small>
-                <p>
-                    <small>
-                        Email: <a href="mailto://jamesroyston03@gmail.com">jamesroyston03@gmail.com</a>
-                    </small>
-                </p>
-                <p>
-                    
-                I'm a software engineer in the midwest, working on React.js applications at <span><a href="https://anautics.com/">Anautics</a>.</span> 
-                    My favorite technologies right now are <a href="https://reactjs.org/">React.js</a>, <a href="https://styled-components.com/">styled components</a>, and <a
-                    href="https://expressjs.com/">Express + Node.js</a>.</p>
+        <hr />
 
-                <hr/>
+        <p>Blog posts:</p>
+        <ul>
+          <li>
+            <a href="https://dev.to/jamesroyston/how-to-deploy-your-express-mongodb-api-react-frontend-to-heroku-4p8m">
+              How to deploy your express-mongoDB API + React frontend to Heroku
+            </a>
+          </li>
+        </ul>
 
-                <p>Blog posts:</p>
-                <ul>
-                    <li><a
-                        href="https://dev.to/jamesroyston/how-to-deploy-your-express-mongodb-api-react-frontend-to-heroku-4p8m">How
-                        to deploy your express-mongoDB API + React frontend to Heroku</a></li>
-                </ul>
-
-            </Body>
-        </Container>
-    )
+        <p>
+          <small>Follow me: </small>
+        </p>
+        <small>
+          <ul>
+            <li>
+              <a href="https://github.com/jamesroyston">Github</a>
+            </li>
+            <li>
+              <a href="https://linkedin.com/in/jtroyston">LinkedIn</a>
+            </li>
+            <li>
+              <a href="https://dev.to/jamesroyston">Dev.to</a>
+            </li>
+          </ul>
+        </small>
+        <p>
+          <small>
+            Email:{" "}
+            <a href="mailto://jamesroyston03@gmail.com">
+              jamesroyston03@gmail.com
+            </a>
+          </small>
+        </p>
+      </Body>
+    </Container>
+  );
 }
